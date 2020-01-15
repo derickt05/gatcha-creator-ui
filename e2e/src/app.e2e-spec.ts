d4f97b5e9ce2e528a850e3777606bc5d0e5c7637
Derick Thompson <derickt05@gmail.com>
2020-01-14 21:55:40 -0500

    rename project

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('gatcha-creator app is running!');
+    expect(page.getTitleText()).toEqual('gatcha-creator-ui app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'gatcha-creator'`, () => {
+  it(`should have as title 'gatcha-creator-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('gatcha-creator');
+    expect(app.title).toEqual('gatcha-creator-ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('gatcha-creator app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('gatcha-creator-ui app is running!');
   });
 });
