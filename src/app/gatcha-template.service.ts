a8d91352780931e8578ecad127f4f12f9713833b
Derick Thompson <derickt05@gmail.com>
2020-01-19 21:37:04 -0500

    turn render into canvas

--- src/app/gatcha-template.service.ts
+++ src/app/gatcha-template.service.ts
@@ -12,16 +12,26 @@ export class GatchaTemplateService {
       live: {
         title: 'Live!',
         template_url: './assets/images/ur_blue_template.png',
-        background_url: 'url("./assets/images/gatcha_background_2.jpg")',
+        background_url: './assets/images/gatcha_background.jpg',
         aspect_ratio: ".71 / 1",
-        width: "512"
+        width: "512",
+        height: "720"
       },
       heroes: {
         title: 'Heroes',
-        template_url: './assets/images/ur_blue_template.png',
-        background_url: 'url("./assets/images/gatcha_background_2.jpg")',
-        aspect_ratio: ".71 / 1",
-        width: "512"
+        template_url: './assets/images/feh_template.png',
+        background_url: './assets/images/feh_background.png',
+        aspect_ratio: "9 / 16",
+        width: "540",
+        height: "960"
+      },
+      the_end: {
+        title: 'The End',
+        template_url: './assets/images/gradius_template.png',
+        background_url: './assets/images/apoc_background.jpg',
+        aspect_ratio: "9 / 16",
+        width: "720",
+        height: "1280"
       }
     }
   }
--- src/app/image-selector/image-selector.component.ts
+++ src/app/image-selector/image-selector.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, Input } from '@angular/core';
 import { ImageCroppedEvent } from 'ngx-image-cropper';
 import { GatchaTemplateService } from '../gatcha-template.service';
-import html2canvas from "html2canvas";
 
 @Component({
   selector: 'app-image-selector',
@@ -10,14 +9,15 @@ import html2canvas from "html2canvas";
 })
 export class ImageSelectorComponent implements OnInit {
 
-  // Gatcha Template properties
+  // Custom Properties
   availableTemplates: any;
   currentTemplate: Object;
+  sources: any;
+  images: any;
+  ctx: CanvasRenderingContext2D;
 
-  // html2canvas properties
-  @ViewChild('screen', {static: false}) screen: ElementRef;
-  @ViewChild('canvas', {static: false}) canvas: ElementRef;
-  @ViewChild('downloadLink', {static: false}) downloadLink: ElementRef;
+  // Custom ViewChildren
+  @ViewChild('heroCanvas', {static: false}) heroCanvas: ElementRef;
 
   // ngx-image-cropper properties
   imageChangedEvent: any = '';
@@ -25,11 +25,16 @@ export class ImageSelectorComponent implements OnInit {
   
   // ngx-image-cropper hooks
   fileChangeEvent(event: any): void {
-      this.imageChangedEvent = event;
+    this.imageChangedEvent = event;
   }
+
   imageCropped(event: ImageCroppedEvent) {
       this.croppedImage = event.base64;
+
+      this.loadSources();
+      this.loadImages(() => this.drawCanvas());
   }
+
   imageLoaded() {
       // show cropper
   }
@@ -40,21 +45,69 @@ export class ImageSelectorComponent implements OnInit {
       // show message
   }
 
-  // html2canvas image download
-  downloadImage(){
-    html2canvas(this.screen.nativeElement).then(canvas => {
-      this.canvas.nativeElement.src = canvas.toDataURL();
-      this.downloadLink.nativeElement.href = canvas.toDataURL('image/png');
-      this.downloadLink.nativeElement.download = 'rendered-image.png';
-      this.downloadLink.nativeElement.click();
+  loadSources() {
+    this.sources = [];
+    this.sources.push(this.currentTemplate['background_url']);
+    if (this.croppedImage !== '') {
+      this.sources.push(this.croppedImage);
+    }
+    this.sources.push(this.currentTemplate['template_url']);
+  }
+
+  loadImages(callback: Function) {
+    this.images = [];
+    var loadedImages = 0;
+    var numImages = this.sources.length;
+    var currentImage = 0;
+    this.sources.forEach(source => {
+      this.images[currentImage] = new Image();
+      this.images[currentImage].onload = () => {
+        if(++loadedImages >= numImages) {
+          callback();
+        }
+      }
+      this.images[currentImage].src = source;
+      currentImage++;
     });
   }
 
+  drawCanvas () {
+    this.images.forEach(image => {
+      this.ctx.drawImage(image, 0, 0, this.currentTemplate['width'], this.currentTemplate['height'])
+    });
+  }
+
+  downloadImage() {
+    alert('I removed html2canvas');
+  }
+
+  changeTemplate(key) {
+    this.currentTemplate = this.availableTemplates[key];
+    this.loadSources();
+    this.loadImages(() => this.drawCanvas());
+  }
+
+  // Constructors/Initializers
   constructor(private _gatchaTemplateService: GatchaTemplateService) { }
 
   ngOnInit() {
     this.availableTemplates = this._gatchaTemplateService.returnTemplates();
-    this.currentTemplate = this.availableTemplates['live'];
+    this.currentTemplate = this.availableTemplates['the_end'];
+  }
+
+  ngAfterViewInit() {
+    this.ctx = this.heroCanvas.nativeElement.getContext('2d');
+    // Load the canvas with the foreground and background even without an uploaded image.
+    this.loadSources();
+    this.loadImages(() => this.drawCanvas());
+  }
+
+  getRatio() {
+    return "9/16";
+  }
+
+  getWidth() {
+    return "720";
   }
 
 }
